e75f637a07a1a18d724a7f233795e4b28413ed85
Aabishkar Aryal <aabiskar1@live.com>
2020-04-23 13:08:51 +0545

    routes fix

--- ApplicationDevGroupWork/App_Start/RouteConfig.cs
+++ ApplicationDevGroupWork/App_Start/RouteConfig.cs
@@ -16,33 +16,47 @@ namespace ApplicationDevGroupWork
             routes.MapRoute(
       name: "Default1",
       url: "Producers/{controller}/{action}/{id}",
-      defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      defaults: new { controller = "Producers", action = "Index", id = UrlParameter.Optional }
    );
             routes.MapRoute(
       name: "Default2",
       url: "Loan/{controller}/{action}/{id}",
-      defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      defaults: new { controller = "Loan", action = "Index", id = UrlParameter.Optional }
    );
 
 
             routes.MapRoute(
               name: "Default3",
               url: "DVDDetails/{controller}/{action}/{id}",
-              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+              defaults: new { controller = "DVDDetails", action = "Index", id = UrlParameter.Optional }
            );
 
             routes.MapRoute(
               name: "Default4",
               url: "CastDetails/{controller}/{action}/{id}",
-              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+              defaults: new { controller = "CastDetails", action = "Index", id = UrlParameter.Optional }
            );
 
 
             routes.MapRoute(
               name: "Default5",
               url: "CastMembers/{controller}/{action}/{id}",
-              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+              defaults: new { controller = "CastMembers", action = "Index", id = UrlParameter.Optional }
            );
+
+
+            routes.MapRoute(
+              name: "Default6",
+              url: "MemberCategories/{controller}/{action}/{id}",
+              defaults: new { controller = "MemberCategories", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+  name: "Default7",
+  url: "Members/{controller}/{action}/{id}",
+  defaults: new { controller = "Members", action = "Index", id = UrlParameter.Optional }
+);
+
 
 
 
